47ebf1983f8234b5b255bf6fb60a77bdf9854091
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-02 01:03:22 -0300

    added general menu

--- app/src/main/java/com/qiubo/vintai/MainActivity.java
+++ app/src/main/java/com/qiubo/vintai/MainActivity.java
@@ -2,12 +2,15 @@ package com.qiubo.vintai;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,22 +26,60 @@ public class MainActivity extends AppCompatActivity {
         setupDrawer();
     }
 
+    @SuppressLint("RestrictedApi")
     private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.vw_home_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.app_name);
     }
 
     private void setupDrawer() {
         mDrawerLayout = findViewById(R.id.vw_home_drawer_layout);
-        NavigationView navigationView = findViewById(R.id.vw_home_navigation_view);
+        NavigationView navigationView = findViewById(R.id.nav_menu);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 mDrawerLayout.closeDrawer(GravityCompat.START);
                 return true;
+            }
+        });
+
+        final NavigationView generalNavigationView = findViewById(R.id.nav_general_menu);
+        generalNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawer(GravityCompat.END);
+                return true;
+            }
+        });
+
+        final AppCompatImageButton generalMenuBtn = findViewById(R.id.btn_right_menu);
+        generalMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+                drawerView.bringToFront();
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
             }
         });
     }
