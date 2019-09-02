ddab3c1c46590118555421a9910f57150f0aa14c
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-02 00:31:16 -0300

    added navigation menu

--- app/src/main/java/com/qiubo/vintai/MainActivity.java
+++ app/src/main/java/com/qiubo/vintai/MainActivity.java
@@ -1,14 +1,50 @@
 package com.qiubo.vintai;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
+        setupDrawer();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.vw_home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.app_name);
+    }
+
+    private void setupDrawer() {
+        mDrawerLayout = findViewById(R.id.vw_home_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.vw_home_navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        mDrawerLayout.openDrawer(GravityCompat.START);
+        return true;
     }
 }
