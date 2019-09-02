4e8b3a0808cd5b1bf38a58a608a4fef87850fac8
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2019-09-02 03:07:53 -0300

    added menu action listener to change screens from general menu

--- app/src/main/java/com/qiubo/vintai/ui/activities/MainActivity.java
+++ app/src/main/java/com/qiubo/vintai/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.qiubo.vintai;
+package com.qiubo.vintai.ui.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,13 +6,18 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.qiubo.vintai.R;
+import com.qiubo.vintai.ui.fragments.PostsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupToolbar();
         setupDrawer();
+    }
+
+    private void setupFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @SuppressLint("RestrictedApi")
@@ -39,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.best_post:
+                        Toast.makeText(MainActivity.this, getString(R.string.lbl_best_posts), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.statistics:
+                        Toast.makeText(MainActivity.this, getString(R.string.lbl_general_statics), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.my_styles:
+                        Toast.makeText(MainActivity.this, getString(R.string.lbl_my_styles), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.best_profiles:
+                        Toast.makeText(MainActivity.this, getString(R.string.lbl_best_profiles), Toast.LENGTH_LONG).show();
+                        break;
+                }
                 mDrawerLayout.closeDrawer(GravityCompat.START);
                 return true;
             }
@@ -48,6 +74,33 @@ public class MainActivity extends AppCompatActivity {
         generalNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        fragment = getFragmentByTag("home");
+                        if(fragment == null)
+                            fragment = PostsFragment.newInstance();
+                        break;
+                    case R.id.take_picture:
+                        fragment = getFragmentByTag("take_picture");
+                        break;
+                    case R.id.api_data:
+                        fragment = getFragmentByTag("api_data");
+                        break;
+                    case R.id.graphic:
+                        fragment = getFragmentByTag("graphic");
+                        break;
+                    case R.id.edit_picture:
+                        fragment = getFragmentByTag("edit_picture");
+                        break;
+                    case R.id.edit_ai:
+                        fragment = getFragmentByTag("edit_ai");
+                        break;
+                }
+
+                if(fragment != null)
+                    setupFragment(fragment);
+
                 mDrawerLayout.closeDrawer(GravityCompat.END);
                 return true;
             }
@@ -82,6 +135,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private Fragment getFragmentByTag(String tag) {
+        return getSupportFragmentManager().findFragmentByTag(tag);
     }
 
     @Override
